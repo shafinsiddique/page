79d9c8b156165805586896cce56ec436f88b8fe9
shafinsiddique <siddiqueshafin20@gmail.com>
2020-12-13 15:26:56 -0500

    mini scanner working

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+//import "fmt"
+
 func main() {
-	fmt.Println("Hello World.")
+	scanner := &BasicScanner{strScan: " (- 2 3) "}
+	tokens := scanner.GetTokens()
+	for _, t := range tokens {
+		fmt.Println(t.ToString())
+	}
+	//fmt.Println(scanner.curIndex)
 }
