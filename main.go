acc653866659b2b72ac255e994f5725af165a25c
shafinsiddique <siddiqueshafin20@gmail.com>
2020-12-14 09:16:47 -0500

    repl working

--- main.go
+++ main.go
@@ -1,17 +1,37 @@
 package main
 
-func main() {
-	scanner := &BasicScanner{strScan: "(+2 (+ 1 1))"}
-	tokens := scanner.GetTokens()
-	parser := &RDParser{}
-	ast := parser.Parse(tokens)
-	ast.Evaluate()
-	//fmt.Println(ast.ToString())
-	//parser.Parse(tokens)
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
+func startRepl(in io.Reader, out io.Writer) {
+	fmt.Println("Welcome to Page - A Minimal Lisp Interpreter Written in Go. Please contact @sl2j for questions.")
+	prompt := "> "
+	scanner := bufio.NewScanner(in)
 
+	for {
+		fmt.Fprint(out, prompt)
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		tokens := (&BasicScanner{strScan: line}).GetTokens()
+		ast := (&RDParser{}).Parse(tokens)
+		ast.Evaluate()
+
+	}
 }
-//	for _, t := range tokens {
-//		fmt.Println(t.ToString())
-//	}
-//}
+func main() {
+	startRepl(os.Stdin, os.Stdout)
+	//scanner := &BasicScanner{strScan: "(+ 2 (+ 1 1)) (+ 1 1)"}
+	//tokens := scanner.GetTokens()
+	//parser := &RDParser{}
+	//ast := parser.Parse(tokens)
+	//ast.Evaluate()
+}
